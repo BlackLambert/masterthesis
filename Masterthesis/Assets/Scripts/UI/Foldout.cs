5e0955cf62b9a77ca9e5dd84326dedf04aff982d
Sebastian Baier <sebastian.baier93@hotmail.de>
2021-08-24 16:45:20 +0200

    Adds foldouts to the menu panel

---
+++ Masterthesis/Assets/Scripts/UI/Foldout.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SBaier.Master
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class Foldout : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField]
+        private GameObject _target;
+        [SerializeField]
+        private bool _foldOutOnStart = true;
+        private bool _foldOut = true;
+        
+        protected virtual void Start()
+		{
+            _foldOut = _foldOutOnStart;
+            Fold();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+		{
+            _foldOut = !_foldOut;
+            Fold();
+		}
+
+		private void Fold()
+		{
+            _target.SetActive(_foldOut);
+		}
+	}
+}
